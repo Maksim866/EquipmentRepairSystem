b0ccd622bbbbddc423567ba476124dbce2752765
kalin <kalinkinmaks2000@gmail.com>
2026-09-07 22:23:32 +0300

    Сделал первую миграцию и скачал dotnet ef

---
+++ EquipmentRepairSystem/EquipmentRepairSystem.Entities.Configurations/IEntitiesAnchor.cs
@@ -0,0 +1,9 @@
+namespace EquipmentRepairSystem.Entities.Configurations;
+
+/// <summary>
+/// Якорный интерфейс для конфигураций сущностей.
+/// </summary>
+public interface IEntitiesAnchor
+{
+
+}
